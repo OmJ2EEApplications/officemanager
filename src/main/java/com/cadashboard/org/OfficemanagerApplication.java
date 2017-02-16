d0e71eb44a389dd1cbd5b6f3a9bf292711798b42
omcrest <om.bhakre@springernature.com>
2017-02-16 19:21:34 +0530

    first commit

---
+++ src/main/java/com/cadashboard/org/OfficemanagerApplication.java
@@ -0,0 +1,12 @@
+package com.cadashboard.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficemanagerApplication.class, args);
+	}
+}
